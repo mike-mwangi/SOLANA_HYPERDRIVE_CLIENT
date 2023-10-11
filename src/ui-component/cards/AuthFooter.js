854cb4e22bf504dc8341972b831139dc237691fd
Mike Thuita <mthuita@verst.earth>
2023-10-11 09:49:30 +0300

    Back to home redirect

--- src/ui-component/cards/AuthFooter.js
+++ src/ui-component/cards/AuthFooter.js
@@ -5,10 +5,10 @@ import { Link, Typography, Stack } from '@mui/material';
 
 const AuthFooter = () => (
     <Stack direction="row" justifyContent="space-between">
-        <Typography variant="subtitle2" component={Link} href="#" target="_blank" underline="hover">
+        <Typography variant="subtitle2" component={Link} href="#" underline="hover">
             solana hyperdrive
         </Typography>
-        <Typography variant="subtitle2" component={Link} href="#" target="_blank" underline="hover">
+        <Typography variant="subtitle2" component={Link} href="#" underline="hover">
             &copy; carbonmarketsledger.com
         </Typography>
     </Stack>
